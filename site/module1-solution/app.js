481b21f61bf3b36b9d9096acb1d2c36d9ccda6af
Wismin Effendi <wismin.effendi@gmail.com>
2016-10-13 19:50:05 -0500

    solution module1 updated

--- site/module1-solution/app.js
+++ site/module1-solution/app.js
@@ -12,17 +12,17 @@ function LunchCheckController($scope) {
 
   $scope.checkOrder = function () {
     var lunchOrders = $scope.lunchOrders;
-    // default to error if no entry
-    $scope.customFontColor = "red";
-    if (lunchOrders === "") {
-      $scope.checkLunchMessage = "Please enter data first";
-      return;
-    }
-    // if no error then font color is green
+
+    // default to green
     $scope.customFontColor = "green";
     var orders = $scope.lunchOrders.split(',')
                   .filter(function(str) {return str.trim().length>0});
-    if (orders.length <= 3) {
+    if (orders.length == 0) {
+      // empty data so make it red
+      $scope.customFontColor = "red";
+      $scope.checkLunchMessage = "Please enter data first";
+    }
+    else if (orders.length <= 3) {
       $scope.checkLunchMessage = "Enjoy!";
     } else {
       $scope.checkLunchMessage = "Too much!";
